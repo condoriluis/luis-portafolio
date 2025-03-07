ca4a111f1fe87fd28a83640e20377ff4ebbbb5c1
luis.condori <luis.condori@produccion.gob.bo>
2025-03-07 11:30:09 -0400

    Logo de mi portafolio actualizado

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,12 +32,13 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com/" />
         <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="anonymous" />
-        
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/assets/images/favicon.png" type="image/png" />
       </head>
+
       <body className={`home-page`}>
         {children}
       </body>
